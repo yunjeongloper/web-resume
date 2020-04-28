ef6ff421e3323d795f14788778b62119384cc7a1
veronica <vero911@naver.com>
2020-04-28 13:32:55 +0900

    v0.0.1 resume commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const morePresentsList = document.querySelector(".js-presents-more"),
+    moreButton = document.querySelector(".js-more-button");
+    moreButtonTitle = moreButton.querySelector("span");
+
+const MORE_BT = "SHOW MORE PRESENTS";
+const LESS_BT = "SHOW LESS PRESENTS";
+
+function handleClickMore() {
+    const buttonTitle = moreButtonTitle.innerHTML;
+    morePresentsList.classList.toggle("show");
+    moreButtonTitle.innerHTML = buttonTitle === MORE_BT ? LESS_BT : MORE_BT;
+}
+
+moreButton.addEventListener("click", handleClickMore);
